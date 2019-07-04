3c7df05fcdc243f1dc661706c1834af3545e80e8
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-07-04 14:26:38 +0800

    Add validation when save new data

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Country;
 
+use Validator;
 use App\Models\CountryModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -24,6 +25,15 @@ class CountryController extends Controller
 
     public function countrySave(Request $request)
     {
+        $rules = [
+            'name' => 'required|min:3',
+            'iso'  => 'required|min:2|max:2',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400); //400 :Server tidak mengerti perintah karena data tidak valid
+        }
+
         $country = CountryModel::create($request->all());
         return response()->json($country, 201); // 201 : Created
     }
