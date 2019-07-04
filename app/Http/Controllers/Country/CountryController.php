54edd0751086e1b4f6a279c8fac6d333612d68cb
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-07-04 11:48:48 +0800

    Create response to save new country

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers\Country;
 
 use App\Models\CountryModel;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CountryController extends Controller
 {
     public function country()
     {
-        return response()->json(CountryModel::get(), 200);
+        return response()->json(CountryModel::get(), 200); // 200 :OK
     }
 
     public function countryByID($id)
     {
-        return response()->json(CountryModel::find($id), 200);
+        return response()->json(CountryModel::find($id), 200); // 200 :OK
+    }
+
+    public function countrySave(Request $request)
+    {
+        $country = CountryModel::create($request->all());
+        return response()->json($country, 201); // 201 : Created
     }
 }
